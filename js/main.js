7ad85ec11481a3715272b40e252ff0e6c13913d2
amiravni16 <amiravni13@gmail.com>
2025-04-24 20:24:46 +0300

    Create meme.controller, meme.service,

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Meme Generator loaded')
---
+++ js/meme-controller.js
@@ -0,0 +1,32 @@
+'use strict'
+
+function initMemeController() {
+    renderMeme()
+}
+
+function renderMeme() {
+    const meme = getMeme()
+    const imgData = getImageById(meme.selectedImgId)
+    
+    const canvas = document.getElementById('meme-canvas')
+    const ctx = canvas.getContext('2d')
+
+   
+    const img = new Image()
+    img.src = imgData.url
+    img.onload = () => {
+        
+        canvas.width = img.width
+        canvas.height = img.height
+
+       
+        ctx.drawImage(img, 0, 0)
+
+      
+        const line = meme.lines[meme.selectedLineIdx]
+        ctx.font = `${line.size}px Arial`
+        ctx.fillStyle = line.color
+        ctx.textAlign = 'center'
+        ctx.fillText(line.txt, canvas.width / 2, 50)
+    }
+}
