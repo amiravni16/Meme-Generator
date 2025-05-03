4f84e676cd6e3f2da6db8b4164e035d7fb1f137f
amiravni16 <amiravni13@gmail.com>
2025-05-03 20:16:25 +0300

    Add I'm flexible button

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -3,40 +3,84 @@
 function initGalleryController() {
     console.log('initGalleryController called')
     renderGallery()
+    initSearchBar()
+    initFlexibleBtn()
 }
 
 function renderGallery() {
     console.log('renderGallery called')
-    const gallery = document.getElementById('gallery-content')
-    const imgs = getImages()
-    console.log('Images to render:', imgs)
-    
-    let htmlStr = ''
-    imgs.forEach(img => {
-        const imgHtml = `
-            <div class="gallery-item">
-                <img src="${img.url}" alt="Meme Image ${img.id}" onclick="onImgSelect(${img.id})">
-            </div>
-        `
-        console.log('Generated HTML for image:', img.id, imgHtml)
-        htmlStr += imgHtml
-    })
-    
-    console.log('Combined HTML:', htmlStr)
-    gallery.innerHTML = htmlStr
-    console.log('Gallery HTML set:', gallery.innerHTML)
-    
-    setTimeout(() => {
-        const imgElements = gallery.querySelectorAll('img')
-        console.log('Number of images in DOM:', imgElements.length)
-        imgElements.forEach(img => {
-            console.log('Image in DOM:', img.src, 'Natural width:', img.naturalWidth)
-        })
-    }, 1000)
+    const images = getImages()
+    const galleryContent = document.getElementById('gallery-content')
+    const strHTMLs = images.map(img => `
+        <div class="gallery-item">
+            <img src="${img.url}" alt="Meme image ${img.id}" onclick="onImageSelect(${img.id})">
+        </div>
+    `)
+    galleryContent.innerHTML = strHTMLs.join('')
 }
 
-function onImgSelect(imgId) {
-    console.log('onImgSelect called with imgId:', imgId)
+function initSearchBar() {
+    const searchInput = document.querySelector('.search-bar input')
+    searchInput.addEventListener('input', () => {
+        const keyword = searchInput.value.toLowerCase()
+        const images = getImages()
+        const filteredImages = images.filter(img => 
+            img.keywords.some(kw => kw.toLowerCase().includes(keyword))
+        )
+        const galleryContent = document.getElementById('gallery-content')
+        const strHTMLs = filteredImages.map(img => `
+            <div class="gallery-item">
+                <img src="${img.url}" alt="Meme image ${img.id}" onclick="onImageSelect(${img.id})">
+            </div>
+        `)
+        galleryContent.innerHTML = strHTMLs.join('')
+    })
+}
+
+function initFlexibleBtn() {
+    const flexibleBtn = document.getElementById('flexible-btn')
+    flexibleBtn.addEventListener('click', () => {
+        generateRandomMeme()
+        document.getElementById('gallery').style.display = 'none'
+        document.getElementById('editor').style.display = 'block'
+        renderMeme()
+    })
+}
+
+function generateRandomMeme() {
+    const images = getImages()
+    const randomImg = images[Math.floor(Math.random() * images.length)]
+    const randomTexts = [
+        'LOL THIS IS FUNNY',
+        'WHEN LIFE GETS TOUGH',
+        'I CAN’T EVEN',
+        'WHY IS THIS ME',
+        'MEME LIFE',
+        'TOO REAL',
+        'SEND HELP',
+        'I’M DONE'
+    ]
+    const randomText = randomTexts[Math.floor(Math.random() * randomTexts.length)]
+    
+    setImg(randomImg.id)
+    const meme = getMeme()
+    meme.lines = [{
+        txt: randomText,
+        size: 20,
+        color: '#ff0000',
+        x: 250,
+        y: 50,
+        boxX: 0,
+        boxY: 0,
+        boxWidth: 0,
+        boxHeight: 0,
+        fontFamily: 'Arial',
+        align: 'center'
+    }]
+    meme.selectedLineIdx = 0
+}
+
+function onImageSelect(imgId) {
     setImg(imgId)
     document.getElementById('gallery').style.display = 'none'
     document.getElementById('editor').style.display = 'block'
